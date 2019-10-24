2f40a1a83c9b430d298de37f7da1594324174c58
yixin_jiang <51935871+yxinjiang@users.noreply.github.com>
2019-10-24 14:01:50 +0800

    Update myutils.py

--- myutils.py
+++ myutils.py
@@ -52,6 +52,3 @@ plt.title('skimage read')
 plt.imshow(im4)
 #plt.axis('off') # 不显示坐标轴
 plt.show()
-————————————————
-版权声明：本文为CSDN博主「walter_xh」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/renelian1572/article/details/78761278
